ea5102e9d760817c7c152e175b6565c87612b78a
Rami Atrouni <ramiatrouni@gmail.com>
2024-04-06 21:59:33 +0300

    attach mongodb to the app through DI + env variables

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,8 +1,13 @@
 import { Application } from '@ubio/framework';
+import { MongoDb } from '@ubio/framework/modules/mongodb';
+import { dep } from 'mesh-ioc';
 
 export class App extends Application {
+    @dep() private mongodb!: MongoDb;
+
     override createGlobalScope() {
         const mesh = super.createGlobalScope();
+        mesh.service(MongoDb);
         return mesh;
     }
 
@@ -12,6 +17,7 @@ export class App extends Application {
     }
 
     override async beforeStart() {
+        await this.mongodb.start();
         await this.httpServer.startServer();
     }
 
@@ -21,6 +27,7 @@ export class App extends Application {
 
     override async afterStop() {
         await this.httpServer.stopServer();
+        await this.mongodb.stop();
     }
 
 }
